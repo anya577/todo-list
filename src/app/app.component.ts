338a99e194cef40164f046d51b97b08a2780f8cf
Anna Tukvadze <annatukvadze@yahoo.com>
2025-02-17 20:01:23 +0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, model, signal, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+interface Task { 
+  id: number;
+  text: string;
+} 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None 
 })
 export class AppComponent {
-  title = 'myList';
+  tasks = signal<Task[]>([]);
+  taskText = model('');
+
+
+  addTask() { 
+    this.tasks.update(tasks => [
+      ...tasks,
+      { id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1, text:this.taskText() }
+    ]);
+    this.taskText.set('');
+  }
+    
+ 
+  removeTask(index: number) { 
+    this.tasks.update(tasks => { 
+      tasks.splice(index, 1)
+      return tasks
+    })
+  }
 }
